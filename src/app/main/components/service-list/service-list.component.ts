eba713887686c1dcf84cc767e60d65665dbc3958
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-10 17:21:56 +0300

    feat: add reactive forms

--- src/app/main/components/service-list/service-list.component.ts
+++ src/app/main/components/service-list/service-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { IUser, IRequest, IService } from './../../../core/models/request.model';
 
 @Component({
@@ -10,11 +11,30 @@ export class ServiceListComponent implements OnInit {
   @Input() data: IRequest;
   @Input() user: IUser;
   services: IService[];
-  value = '';
+  serviceForm = this.fb.group({
+    serviceName: [''],
+    clearButton: [''],
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.services = this.data.services;
+  }
+
+  onSubmit(): void {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.serviceForm.value);
+  }
+
+  get aliases(): FormArray {
+    return this.serviceForm.get('aliases') as FormArray;
+  }
+
+  addAlias(): void {
+    this.aliases.push(this.fb.control(''));
   }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IUser } from '../core/models/request.model';
 import { DataService } from '../core/services/data.service';
 
@@ -10,10 +11,24 @@ import { DataService } from '../core/services/data.service';
 })
 export class SettingsComponent implements OnInit {
   user: IUser;
+  settingsForm: FormGroup;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.user = this.dataService.readUser();
+    this.settingsForm = new FormGroup({
+      userName: new FormControl({value: this.user.name, disabled: true}, Validators.maxLength(200)),
+      psevdo: new FormControl(this.user.name, Validators.maxLength(200)),
+      notification: new FormGroup({
+        enableNotification: new FormControl('checked'),
+        // enableEmail: new FormControl({value: 'email', checked: true}),
+        // email: new FormControl('mail@mail.com'),
+        // enablePhone: new FormControl({value: 'phone', checked: false}),
+        // phone: new FormControl('89000000000')
+      }),
+      // discard: new FormControl(),
+      // save: new FormControl()
+    });
   }
 }
